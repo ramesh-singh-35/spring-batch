91f858332e3553624eaa6a056d0a855bf14b1235
Ramesh Singh <ramesh.coder.87@gmail.com>
2022-06-05 08:06:46 -0500

    Initial project set up fot spring batch partition demo.

---
+++ spring-batch-partition-demo/src/main/java/com/alphacoder/SpringBatchPartitionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPartitionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPartitionDemoApplication.class, args);
+	}
+
+}
